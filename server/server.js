4630bf63cd8788bb36dd112e0a83936ca92596a0
Isarel Getu <israelgetu101@gmail.com>
2022-03-16 17:40:39 -0500

    neww

--- server/server.js
+++ server/server.js
@@ -7,39 +7,46 @@ app.use(cors());
 app.use(express.json());
 
 //Middleware
-app.use(express.static(path.join(__dirname, "../public")));
+// app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(
-  "/image/bookstore",
-  express.static(path.join(__dirname, "../image/bookstore.jpg"))
-);
-app.use(
-  "/image/bookstore1",
-  express.static(path.join(__dirname, "../image/book2.jpg"))
-);
-app.use(
-  "/image/bookstore2",
-  express.static(path.join(__dirname, "../image/book3.jpg"))
-);
+// app.use(
+//   "/image/bookstore",
+//   express.static(path.join(__dirname, "../image/bookstore.jpg"))
+// );
+// app.use(
+//   "/image/bookstore1",
+//   express.static(path.join(__dirname, "../image/book2.jpg"))
+// );
+// app.use(
+//   "/image/bookstore2",
+//   express.static(path.join(__dirname, "../image/book3.jpg"))
+// );
 
 //Endpoints
 
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../public"));
-// });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public"));
+});
 
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../public/index.html"));
-// });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-// app.get("/styles", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../public/index.css"));
-// });
+app.get("/styles", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.css"));
+});
 
-// app.get("/image/bookstore", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../image/bookstore.jpg"));
-// });
+app.get("/image/bookstore", function (req, res) {
+  res.sendFile(path.join(__dirname, "../image/bookstore.jpg"));
+});
 
+app.get("/image/bookstore1", function (req, res) {
+  res.sendFile(path.join(__dirname, "../image/book1.jpg"));
+});
+
+app.get("/image/bookstore2", function (req, res) {
+  res.sendFile(path.join(__dirname, "../image/book2.jpg"));
+});
 //port
 const port = process.env.PORT || 4005;
 app.listen(port, () => {
